6591ef9b6441a34fbd9ed8afffc1f92b54127c0b
Mike Basdeo <mikebasdeo@gmail.com>
2019-02-24 15:55:50 -0500

    Camera videostream works!

--- OpenVideo.py
+++ OpenVideo.py
@@ -1,3 +1,16 @@
 import cv2
-import numpy
-cap = cv2.VideoCapture(1)
+
+video = cv2.VideoCapture(0)
+video.set(cv2.CAP_PROP_FRAME_WIDTH,640);
+video.set(cv2.CAP_PROP_FRAME_HEIGHT,480);
+
+while True:
+    check, frame = video.read()
+    cv2.imshow('Video window', frame)
+
+    # press 'q' on keyboard to exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
