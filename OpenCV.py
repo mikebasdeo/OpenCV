7af5777d91ec968ef63e53c0ec8594931a9818f1
Mike Basdeo <mikebasdeo@gmail.com>
2019-02-24 14:12:47 -0500

    Image is opened, and then output as a png using OpenCV.

--- OpenCV.py
+++ OpenCV.py
@@ -12,12 +12,25 @@ img = cv2.imread('mri.jpg', cv2.IMREAD_GRAYSCALE)
 
 print('test')
 
+
+# --------------- OPENING IMAGES
+
+# open an image using matplotlib
+# plt.imshow(img, cmap='gray', interpolation='bicubic')
+# plt.plot([500,55], [55,500], 'c', linewidth=5)
+# plt.show()
+
 # open an image using OpenCV
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# open an image using matplotlib
-plt.imshow(img, cmap='gray', interpolation='bicubic')
-plt.show()
+# --------------- SAVING IMAGES
+
+# save an image using OpenCV
+cv2.imwrite('output.png', img)
+
+
+
+
 
